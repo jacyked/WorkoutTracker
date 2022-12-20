3d397558754499ccf6cb1cc5e9caf012b8fa521b
Matt Jacyk <=>
2022-12-20 01:31:52 -0700

    Reformat for trackworkout flow, add stepper and placeholders for steps

--- client/src/components/user/TrackWorkout.js
+++ client/src/components/user/TrackWorkout.js
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from "react";
+import { Outlet } from "react-router-dom"
 import useAxiosPrivate from "../../hooks/useAxiosPrivate";
 import useAuth from '../../hooks/useAuth';
-
-
 import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
@@ -11,54 +10,43 @@ import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
 import { AppSideBar } from '../layout/AppSideBar.js';
+import { useTheme } from '@mui/material/styles';
+import MobileStepper from '@mui/material/MobileStepper';
+import Button from '@mui/material/Button';
+import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
 
 
-const USER_URL="/user";
 const TrackWorkout = () => {
+  const theme = useTheme();
+  const [activeStep, setActiveStep] = React.useState(0);
 
-    const [thisUser, setUser] = useState();
-    const axiosPrivate = useAxiosPrivate();
-      const { auth } = useAuth();
-  
-  
-  
-    useEffect(() => {
-      let isMounted = true;
-      const controller = new AbortController();
-      console.log(JSON.stringify(auth));
-      if(!auth?.currUser){
-        const getUser = async() => {
-            try{
-                //All User routes removed the signal in axios request. may want to reinstate
-                const response = await axiosPrivate.get(USER_URL, {
-                    signal: controller.signal
-                });
-                console.log("Fired: Track");
-                console.log("DATA: " + JSON.stringify(response.data));
-                isMounted && setUser(response.data);
-                //setAuth({...auth.prev, curUser: response.data});
-                //console.log("User: " + JSON.stringify(response.data));
-                
-            }catch(err){
-                console.error(err);
-            }
-  
-        }
-        getUser();
-        }else{
-            setUser(auth.currUser);
-        }
-      return () => {
-            console.log("cleanup, aborting track axios. ");
-          isMounted = false;
-          controller.abort();
-      }
-    }, [])
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
 
-    return(
-        <Box sx={{ display: 'flex' }}>
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const RenderStep = () => {
+    switch(activeStep){
+      case 0:
+        return(<Typography variant="h5">Step 0</Typography>);
+        break;
+      case 1:
+        return(<Typography variant="h6">Step 1</Typography>);
+        break;
+      default:
+        return(<Typography variant="h4">Ki has small PP</Typography>);
+    }
+  }
+
+    return (
+      <React.Fragment>
+      <Box sx={{ display: 'flex' }}>
         <CssBaseline />
-        <AppSideBar title="Track a Workout"/>
+        <AppSideBar title = "Track a Workout"/>
         <Box
           component="main"
           sx={{
@@ -73,46 +61,49 @@ const TrackWorkout = () => {
         >
           <Toolbar />
           <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
-            <Grid container spacing={3}>
-              {/* COMPONENT 1 */}
-              <Grid item xs={12} md={8} lg={9}>
                 <Paper
                   sx={{
-                    p: 2,
+                    p: {xs: 'none', md: 1, lg: 2},
                     display: 'flex',
                     flexDirection: 'column',
-                    height: 240,
                   }}
                 >
-                    <br />
-                    <Typography component="p" variant="subtitle2">
-                    Track Workout here
-                    </Typography>
-                  {/*  ADD COMPONENT 1 HERE  */}
+                    <RenderStep />
+                     <MobileStepper
+                      variant="progress"
+                      steps={6}
+                      position="static"
+                      activeStep={activeStep}
+                      sx={{ maxWidth: 400, flexGrow: 1 }}
+                      nextButton={
+                        <Button size="small" onClick={handleNext} disabled={activeStep === 5}>
+                          Next
+                          {theme.direction === 'rtl' ? (
+                            <KeyboardArrowLeft />
+                          ) : (
+                            <KeyboardArrowRight />
+                          )}
+                        </Button>
+                      }
+                      backButton={
+                        <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
+                          {theme.direction === 'rtl' ? (
+                            <KeyboardArrowRight />
+                          ) : (
+                            <KeyboardArrowLeft />
+                          )}
+                          Back
+                        </Button>
+                      }
+                    />
                 </Paper>
-              </Grid>
-              {/* COMPONENT 2 */}
-              <Grid item xs={12} md={4} lg={3}>
-                <Paper
-                  sx={{
-                    p: 2,
-                    display: 'flex',
-                    flexDirection: 'column',
-                    height: 240,
-                  }}
-                >
-                  {/* ADD COMPONENT 2 */}
-                </Paper>
-              </Grid>
-            </Grid>
-
-              
-              
           </Container>
         </Box>
-      </Box>
-    );
-
+      </Box> 
+    </React.Fragment>
+    )
 }
+
+
 
 export default TrackWorkout;
