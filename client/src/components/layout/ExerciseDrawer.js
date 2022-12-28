e4bff2203ff3be3ac88036aeb548e5571e0abb51
Matt Jacyk <=>
2022-12-28 09:36:12 -0700

    add weight and sets to exercise drawer

--- client/src/components/layout/ExerciseDrawer.js
+++ client/src/components/layout/ExerciseDrawer.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import ListSubheader from '@mui/material/ListSubheader';
 import List from '@mui/material/List';
 import ListItemButton from '@mui/material/ListItemButton';
@@ -13,6 +13,7 @@ import ExpandMore from '@mui/icons-material/ExpandMore';
 import StarBorder from '@mui/icons-material/StarBorder';
 import TextField from '@mui/material/TextField';
 import { IconButton } from '@mui/material';
+import AddBoxIcon from '@mui/icons-material/AddBox';
 
 
 export default function ExerciseDrawer(props) {
@@ -20,12 +21,27 @@ export default function ExerciseDrawer(props) {
     const length = props.length;
     const index = props.exercise.index;
     const name = props.exercise.name;
-    const sets = props.exercise.sets;
+    const [sets, setSets] = useState(props.exercise.sets);
+    const [weight, setWeight] = useState(0);
+    const [reps, setReps] = useState(0);
     const [open, setOpen] = useState((index === (length - 1))?true:false);
   
     const handleClick = () => {
       setOpen(!open);
     };
+
+    function addSet(){
+        console.log("Add set triggered");
+        let arr = sets;
+        if(arr[0].reps <= 0)
+            arr = [{weight, reps}]
+        else
+            arr.push({weight, reps});
+        setSets(sets);
+        
+        //Update local storage at this point.
+        
+    }
 
 return (
 <List dense={true}>
@@ -34,9 +50,23 @@ return (
     {open ? <ExpandLess /> : <ExpandMore />}
     </ListItemButton>
     <Collapse in={open} timeout="auto" unmountOnExit>
-        <List component="div" disablePadding inset={true}>
+        <List component="div" disablePadding inset={true} dense={true}>
+        
+        {(!Array.isArray(sets))? (
+                <React.Fragment></React.Fragment>
+            ):(!(sets[0].reps <= 0))?(
+                sets.map((set) => (
+                    <ListItemText primary={set.weight + " x " + set.reps}/>
+                ))
+                
+            ): (
+                <React.Fragment></React.Fragment>
+            )}
             <ListItemButton sx={{ pl: 4 }}>
-                <TextField type="number" label="weight"/> x <TextField type="number" label="reps" /> <IconButton />
+                <TextField type="number" label="weight" value={weight} onChange={(e) => {setWeight(e.target.value)}}/> x <TextField type="number" label="reps" value={reps} onChange={(e) => {setReps(e.target.value)}}/>
+                <IconButton edge="end" aria-label="add" colour="primary" onClick={() => {addSet(); console.log("Saving weight " + weight + " x reps " + reps)}}>
+                                <AddBoxIcon />
+                </IconButton> 
             </ListItemButton>
         </List>
     </Collapse>
